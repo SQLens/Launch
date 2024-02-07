9ea820a5769a95f06e6ded091659a81bf9fd9bc4
Jarod Crawford <jarodcrawford@Jarods-Air.attlocal.net>
2024-02-07 14:11:58 -0600

    fixes github redirect

--- components/navbar.js
+++ components/navbar.js
@@ -85,7 +85,7 @@ const Navbar = () => {
         </div>
 
         <div className="hidden mr-3 space-x-4 lg:flex nav__item">
-  <Link href="/get-started" passHref>
+  <Link href="https://github.com/oslabs-beta/SQLens" passHref>
     <span className="relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
       <span className="relative px-11 py-3 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
         Get Started
