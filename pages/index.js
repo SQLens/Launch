66873a6cf3f1a3b3edbdfc80b7a382c9bb5bb621
Jarod Crawford <jarodcrawford@Jarods-Air.attlocal.net>
2024-02-07 18:29:07 -0600

    small fixes

--- pages/index.js
+++ pages/index.js
@@ -37,8 +37,6 @@ const Home = () => {
       </SectionTitle>
       <Video />
       <SectionTitle pretitle="FAQ" title="Frequently Asked Questions">
-        Answer your customers possible questions here, it will increase the
-        conversion rate as well as support or chat requests.
       </SectionTitle>
       <Faq />
       {/* <Cta /> */}
